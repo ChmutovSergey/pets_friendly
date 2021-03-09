15c04ea0b89c678533995840dcae014755844f02
ChmutovSergey <chmutovsergey@gmail.com>
2021-03-09 22:21:06 +0300

    Обновить модели под новую структуру

--- main_app/models.py
+++ main_app/models.py
@@ -2,57 +2,46 @@ from django.db import models
 
 
 class Contact(models.Model):
-    __tablename__ = 'contact'
-
-    adress = models.CharField(max_length=500, null=False)
-    phone = models.BigIntegerField(null=False)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=False)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=False)
+    address = models.CharField(max_length=500, null=False, verbose_name='Адрес')
+    phone = models.BigIntegerField(null=False, verbose_name='Номер телефона')
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=False, verbose_name='Долготоа')
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=False, verbose_name='Широта')
 
     def __str__(self):
-        return self.adress
+        return self.address
 
 
 class Hotel(models.Model):
-    __tablename__ = 'hotel'
-
-    title = models.CharField(max_length=200, null=False)
-    description = models.TextField()
-    id_contacts = models.ForeignKey('main_app.Contact', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=False, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    contact = models.ForeignKey('main_app.Contact', on_delete=models.CASCADE, verbose_name='Контакты')
 
     def __str__(self):
         return self.title
 
 
 class Room(models.Model):
-    __tablename__ = 'room'
-
-    uuid = models.UUIDField(primary_key=True, null=False)
-    title = models.CharField(max_length=200, null=False)
-    description = models.TextField()
-    area = models.FloatField(null=False)
-    id_hotel = models.ForeignKey('main_app.Hotel', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=False, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    area = models.FloatField(null=False, verbose_name='Площадь')
+    hotel = models.ForeignKey('main_app.Hotel', on_delete=models.PROTECT, verbose_name='Отель')
 
     def __str__(self):
         return self.title
 
 
 class RoomDetail(models.Model):
-    __tablename__ = 'room_details'
-
-    title = models.CharField(max_length=200, null=False)
-    seats = models.IntegerField(null=False)
-    rate = models.FloatField(null=False)
-    id_room = models.ForeignKey('main_app.Room', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=False, verbose_name='Наименование')
+    seats = models.IntegerField(null=False, verbose_name='Количество мест')
+    rate = models.FloatField(null=False, verbose_name='Цена')
+    room = models.ForeignKey('main_app.Room', on_delete=models.PROTECT, verbose_name='Номер')
 
     def __str__(self):
         return self.title
 
 
 class PetsType(models.Model):
-    __tablename__ = 'pets_type'
-
-    pets_type = models.CharField(max_length=500, primary_key=True, null=False)
+    pets_type = models.CharField(max_length=500, primary_key=True, null=False, verbose_name='Вид животного')
 
     def __str__(self):
         return self.pets_type
