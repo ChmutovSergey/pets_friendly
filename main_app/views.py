7e01ab4f2bde829dd1e3a30b41eab68cb9767582
ChmutovSergey <chmutovsergey@gmail.com>
2021-04-04 20:25:05 +0300

    Добавить страницу с отелями

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,14 @@ def index_page(request):
     data = {'yandex_map_api_key': YANDEX_MAP_API_KEY}
 
     return render(request, 'main_app/index.html', context=data)
+
+
+def hotels_page(request):
+    data = {'yandex_map_api_key': YANDEX_MAP_API_KEY}
+
+    latitude = request.GET.get('latitude')
+    longitude = request.GET.get('longitude')
+
+    print("RESULT  ", latitude, longitude)
+
+    return render(request, 'main_app/hotels.html', context=data)
