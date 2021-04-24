c44a718bbcb3f1215569b9b96428975e005b9b21
schmutov <chmutovsergey@gmail.com>
2021-04-24 08:27:16 +0300

    Добавить роут privacy

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('hotels/', views.hotels_page, name='hotels'),
     # path('hotels/<int:hotel_id>/', views.card_hotel_page, name='card_hotel'),
     path('help/', views.help_page, name='help'),
+    path('privacy/', views.privacy_page, name='privacy'),
 ]
