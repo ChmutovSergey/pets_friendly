eff69850643364846665522162e74de22783c64a
schmutov <chmutovsergey@gmail.com>
2021-05-01 10:21:53 +0300

    Добавить отправку сообщения

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,19 @@
+from email.header import Header
+from email.mime.text import MIMEText
+from configparser import ConfigParser
+import os
 from math import sqrt
+import smtplib
 
 from django.shortcuts import render
 from main_app.models import Hotel
 from .controller import YANDEX_MAP_API_KEY, format_phone_number, get_work_time
+
+
+CONFIGS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config')
+MAIL_CONFIG_PATH = os.path.join(CONFIGS_PATH, 'mail_conf.ini')
+print("CONFIGS_PATH", CONFIGS_PATH)
+print("MAIL_CONFIG_PATH", MAIL_CONFIG_PATH)
 
 
 def index_page(request):
@@ -45,27 +56,39 @@ def hotels_page(request):
 
 
 def help_page(request):
+    if not request.GET:
+        return render(request, 'main_app/help.html')
+
+    cfg = ConfigParser()
+    try:
+        cfg.read(MAIL_CONFIG_PATH)
+    except Exception:
+        return
+
+    name = request.GET.get('name')
+    email = request.GET.get('email')
+    message = request.GET.get('msg')
+    server, port = cfg.get('smtp_server', 'server'), cfg.get('smtp_server', 'port')
+    send_mail, send_password = cfg.get('send_mail', 'mail'), cfg.get('send_mail', 'password')
+    recipients_emails = cfg.get('recipients_emails', 'mail')
+
+    if name and email and message:
+        send_message = f'Отправитель: {name}\ne-mail: {email}\nСообщение: {message}'
+        msg = MIMEText(send_message, 'plain', 'utf-8')
+        msg['Subject'] = Header('Центр поддержки', 'utf-8')
+        msg['From'] = send_mail
+        msg['To'] = recipients_emails
+
+        smtp_obj = smtplib.SMTP(server, 587, timeout=10)
+        try:
+            smtp_obj.starttls()
+            smtp_obj.login(send_mail, send_password)
+            smtp_obj.sendmail(send_mail, recipients_emails, msg.as_string())
+        finally:
+            smtp_obj.quit()
+
     return render(request, 'main_app/help.html')
 
 
 def privacy_page(request):
     return render(request, 'main_app/privacy.html')
-
-
-# def card_hotel_page(request, hotel_id):
-#     data = {'yandex_map_api_key': YANDEX_MAP_API_KEY}
-#     try:
-#         hotel = Hotel.objects.select_related().get(id=hotel_id)
-#     except Exception:
-#         return HttpResponse(status=404)
-#
-#     data.update({
-#         'title': hotel.title,
-#         'description': hotel.description,
-#         'address': hotel.address,
-#         'latitude': float(hotel.latitude),
-#         'longitude': float(hotel.longitude),
-#
-#     })
-#
-#     return render(request, 'main_app/card_hotel.html', context=data)
