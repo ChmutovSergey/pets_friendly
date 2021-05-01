49c667fe60ced058b75580d30d191eade6e5938b
schmutov <chmutovsergey@gmail.com>
2021-05-01 10:20:09 +0300

    Удалить url карточки объекта

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index_page, name='index'),
     path('hotels/', views.hotels_page, name='hotels'),
-    # path('hotels/<int:hotel_id>/', views.card_hotel_page, name='card_hotel'),
     path('help/', views.help_page, name='help'),
     path('privacy/', views.privacy_page, name='privacy'),
 ]
