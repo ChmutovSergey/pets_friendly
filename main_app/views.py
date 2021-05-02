38e1dafafcd6ddb82d8350d9e8808f48f2bc10d5
schmutov <chmutovsergey@gmail.com>
2021-05-02 14:46:50 +0300

    Вынести логигу обработки списка отелей в контроллер

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,12 @@
-from math import sqrt
 from django.shortcuts import render
+from os import environ
 
 from main_app.base_logger import logger
-from main_app.controllers.hotels import YANDEX_MAP_API_KEY, format_phone_number, get_work_time
 from main_app.controllers.help import send_mail
-from main_app.models import Hotel
+from main_app.controllers.hotels import get_hotels_list
+
+
+YANDEX_MAP_API_KEY = environ.get('MAP_API_KEY')
 
 
 def index_page(request):
@@ -16,32 +18,13 @@ def hotels_page(request):
     #  пока заданы координаты центра Москвы latitude=37.617635, longitude=55.755814
     base_point = (
         float(request.GET.get('latitude', 55.755814)),
-        float(request.GET.get('longitude', 37.617635))
+        float(request.GET.get('longitude', 37.617635)),
     )
     data = {
         'yandex_map_api_key': YANDEX_MAP_API_KEY,
         'center_map': base_point,
-        'hotels': [],
+        'hotels': get_hotels_list(base_point),
     }
-
-    hotels = Hotel.objects.select_related()
-    if hotels:
-        data_hotels = [
-            {
-                'title': hotel.title,
-                'address': hotel.address,
-                'short_description': hotel.short_description,
-                'phone': format_phone_number(hotel.phone),
-                'work_time': get_work_time(hotel.opening_time, hotel.closing_time),
-                'geo_point': (float(hotel.latitude), float(hotel.longitude)),
-                'metro': [[item.station, item.metro_line.first().img_url] for item in hotel.metro.all()],
-            } for hotel in hotels
-        ]
-        # сортируем объекты по расстоянию от центра карты
-        data_hotels.sort(key=lambda hotel: sqrt(
-            (hotel['geo_point'][0] - base_point[0]) ** 2 + (hotel['geo_point'][1] - base_point[1]) ** 2
-        ))
-        data['hotels'].extend(data_hotels)
 
     return render(request, 'main_app/hotels.html', context=data)
 
@@ -50,15 +33,12 @@ def help_page(request):
     if not request.GET:
         return render(request, 'main_app/help.html')
 
-    name = request.GET.get('name')
-    email = request.GET.get('email')
-    message = request.GET.get('msg')
-
+    name, email, message = request.GET.get('name'), request.GET.get('email'), request.GET.get('msg')
     send_message_status = send_mail(name, email, message)
     if not send_message_status:
         logger.warning('Send mail fail')
-
     logger.info('Send mail success')
+
     return render(request, 'main_app/help.html')
 
 
